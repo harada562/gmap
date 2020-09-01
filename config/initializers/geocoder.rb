e2b7c03fa7d8d1444131678e2093d3f999ec6a27
vagrant <vagrant@localhost.localdomain>
2020-09-01 09:42:34 +0000

    Google API

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -4,7 +4,7 @@ Geocoder.configure(
     lookup: :google,
 
     # to use an API key:
-    api_key: "AIzaSyAMaUX8y-y1Kk4TzBmDmjBIylax5DvqfaE",
+    api_key: ENV['GOOGLE_MAP_API_KEY'],
 
     # geocoding service request timeout, in seconds (default 3):
     timeout: 5,
